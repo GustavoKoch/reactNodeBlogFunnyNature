dc40cd7acdaaff0805a968a4e142b6bf12ca4b31
andre-rost <andrerost86@gmail.com>
2022-06-10 18:10:24 +0200

    Add BlogPosts component
    Co-authored-by: Electric_Sheep <MyElectricSheep@users.noreply.github.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,47 @@
+import instagram from "../Footer/Icons/instagram.png";
+import facebook from "../Footer/Icons/facebook.png";
+import rss from "../Footer/Icons/rss.png";
+
 const Footer = () => {
-    return (
-      <div>
-        <h1></h1>
-        <nav>
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footer-icons">
+          <li>
+            <a href="#">
+              <img src={instagram} alt="Instagram Icon" width="25" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={facebook} alt="Facebook Icon" width="25" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={rss} alt="RSS Icon" width="25" />
+            </a>
+          </li>
+        </div>
+        <div className="footer-links">
           <ul>
-            <li></li>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Copyright</a>
+            </li>
           </ul>
-        </nav>
-      </div>
-    );
-  };
-  
-  export default Footer;
-  
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
